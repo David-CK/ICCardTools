f14ed8999b032cf2c5f15017ebdfd0b0918204dc
David <davidck@126.com>
2020-03-23 19:58:56 +0800

    feat(RF-EYE-U010): add ConnectBtn_Click and DisconnectBtn_Click

--- RF-EYE-U010/App.xaml.cs
+++ RF-EYE-U010/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,14 @@ namespace RF_EYE_U010
     /// </summary>
     public partial class App : Application
     {
+        [DllImport("mwrf32.dll", EntryPoint = "rf_init", SetLastError = true,
+            CharSet = CharSet.Auto, ExactSpelling = false,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr rf_init(Int16 port, Int32 baud);
+
+        [DllImport("mwrf32.dll", EntryPoint = "rf_exit", SetLastError = true,
+            CharSet = CharSet.Auto, ExactSpelling = false,
+            CallingConvention = CallingConvention.StdCall)]
+        public static extern Int16 rf_exit(IntPtr icdev);
     }
 }
--- RF-EYE-U010/MainWindow.xaml.cs
+++ RF-EYE-U010/MainWindow.xaml.cs
@@ -24,5 +24,64 @@ namespace RF_EYE_U010
         {
             InitializeComponent();
         }
+
+        IntPtr icdev = IntPtr.Zero;
+        Int16 port = 0;
+        Int32 baud = 0;
+        Int16 st = 0;
+
+        private void ConnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.AppendText("\n");
+            txtOutput.ScrollToEnd();
+
+            if (icdev.ToInt32() > 0)
+            {
+                txtOutput.AppendText("设备已经连接\n");
+                txtOutput.ScrollToEnd();
+            }
+            else
+            {
+                icdev = App.rf_init(port, baud);
+                if (icdev.ToInt32() > 0)
+                {
+                    txtOutput.AppendText("设备连接成功\n");
+                    txtOutput.ScrollToEnd();
+                }
+                else
+                {
+                    txtOutput.AppendText("设备连接失败\n");
+                    txtOutput.ScrollToEnd();
+                }
+            }
+        }
+
+        private void DisconnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.AppendText("\n");
+            txtOutput.ScrollToEnd();
+
+            if (icdev.ToInt32() > 0)
+            {
+                st = App.rf_exit(icdev);
+                if (st == 0)
+                {
+                    icdev = IntPtr.Zero;
+
+                    txtOutput.AppendText("设备断开成功\n");
+                    txtOutput.ScrollToEnd();
+                }
+                else
+                {
+                    txtOutput.AppendText("设备断开失败\n");
+                    txtOutput.ScrollToEnd();
+                }
+            }
+            else
+            {
+                txtOutput.AppendText("设备已经断开\n");
+                txtOutput.ScrollToEnd();
+            }
+        }
     }
 }
